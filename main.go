e8fec56a86a04a4e99264af9d36e19238115c3fd
Wafiy Anwarul HIkam <95902360+wafiyanwarul@users.noreply.github.com>
2023-07-28 22:11:38 +0700

    memperbarui isi main

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	// ini adalah komentar di GO language
+	// ini adalah tipe komentar inline
 	fmt.Println("Hello, world!")
 	fmt.Println("This is GO Language!")
+
+	// fmt.Println("Pesan ini tidak akan dieksekusi")
 }
