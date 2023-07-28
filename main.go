7e66f0eedcee9a88408bfce96fb4c23c5ff5d011
Wafiy Anwarul HIkam <95902360+wafiyanwarul@users.noreply.github.com>
2023-07-28 22:15:05 +0700

    add multiline comment

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	// ini adalah komentar di GO language
-	// ini adalah tipe komentar inline
+	/*
+	   ini adalah komentar di GO language
+	   ini adalah tipe komentar multiline
+	*/
 	fmt.Println("Hello, world!")
 	fmt.Println("This is GO Language!")
 
 	// fmt.Println("Pesan ini tidak akan dieksekusi")
+	// fmt.Println("ini adalah tipe komentar inline")
 }
